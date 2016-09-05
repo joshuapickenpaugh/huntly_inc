16c68170467917097171189f1c7c8ae83dedc4de
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-05 09:08:04 -0500

    Another commit

--- Huntly_Inc/Form1.vb
+++ Huntly_Inc/Form1.vb
@@ -8,6 +8,26 @@
 
     Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
 
+        'Declares the variables:
+        Dim dblGrossProfitRatio As Double
+        Dim dblGrossProfit As Double
+        Dim dblNetSales As Double
+
+        Dim dblGrossSales As Double
+        Dim dblSalesReturns As Double
+        Dim dblCostOfGoodsSold As Double
+
+        'Assigns variables to text boxes:
+        Double.TryParse(txtCostOfGoodsSold.Text, dblCostOfGoodsSold)
+        Double.TryParse(txtGrossSales.Text, dblGrossSales)
+        Double.TryParse(txtSalesReturns.Text, dblSalesReturns)
+
+        'Calculates:
+        dblNetSales = dblGrossSales - dblSalesReturns
+        dblGrossProfit = dblNetSales - dblCostOfGoodsSold
+        dblGrossProfitRatio = dblGrossProfit / dblNetSales
+
+        'Displays Gross Profit Ratio:
 
     End Sub
 End Class
